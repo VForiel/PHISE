7ff3a2d758610c98dfffe9010e66a3f3410679bc
Vincent Foriel <vince.lrf@gmail.com>
2024-02-28 14:56:25 +0100

    Pretty good convergence 🎉

--- model_v0/utils.py
+++ model_v0/utils.py
@@ -2,6 +2,162 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 
+def bound_phase(phase:np.array) -> np.array:
+    """------------------------------------------------------------------------
+    Bound the phase between [-1,1] (normalized from [-pi,pi])
+
+    Parameters
+    ----------
+    - `phase` : Normalized input phase that can be out of bounds [-1,1]
+
+    Returns
+    -------
+    - Normalized phase bounded in [-1,1]
+    ------------------------------------------------------------------------"""
+    return (phase + 1) % 2 - 1
+
+def phase_shift(beam:complex, dphase:float) -> complex:
+    """------------------------------------------------------------------------
+    De-phase the input beam by heating the fiber with an electrical current.
+    
+    Parameters
+    ----------
+    - `beam` : Input beam complex amplitude
+    - `shift` : Normalized phase shift ([-1,1] ->[-pi,pi])
+    
+    Returns
+    -------
+    - Output beam complex amplitude
+    ------------------------------------------------------------------------"""
+    return beam * np.exp(1j * dphase * np.pi)
+
+def mach_zehnder(beam:complex, dphase:float) -> complex:
+    """------------------------------------------------------------------------
+    Partially or totally cancel the light of a beam by
+    splitting it in two part, dephasing one path and recombining them.
+    
+    Parameters
+    ----------
+    - `input_beam` : Input beam complex amplitude
+    - `input_power` : Input power in Watts for the dephasing.
+    
+    Returns
+    -------
+    - Output beam complex amplitude
+    ------------------------------------------------------------------------"""
+    return (beam + phase_shift(beam, dphase)) / 2
+
+
+def nuller4x4(beams:list[complex]) -> tuple[complex, list[complex]]:
+    """------------------------------------------------------------------------
+    Simulate a 4 input beam nuller.
+    
+    Parameters
+    ----------
+    - `beams` : List of 4 input beams complex amplitudes
+    
+    Returns
+    -------
+    - Bright channel complex amplitude
+    - List of 3 dark channels complex amplitudes
+    ------------------------------------------------------------------------"""
+
+    N = 1/np.sqrt(4) * np.array([
+        [1,  1,  1,  1],
+        [1,  1, -1, -1],
+        [1, -1,  1, -1],
+        [1, -1, -1,  1]
+    ])
+    
+    outputs = N @ beams
+
+    return outputs[0], outputs[1:]
+
+def nuller_2x2(beams:np.array) -> np.array:
+    """------------------------------------------------------------------------
+    Simulate a 2 input beam nuller.
+
+    Parameters
+    ----------
+    - `beams` : Array of 2 input beams complex amplitudes
+
+    Returns
+    -------
+    - Array of 2 output beams complex amplitudes
+        - 1st output is the bright channel
+        - 2nd output is the dark channel
+    ------------------------------------------------------------------------"""
+
+    N = 1/np.sqrt(2) * np.array([
+        [1,   1],
+        [1,  -1],
+    ])
+
+    return N @ beams
+
+def splitmix_4x4(beams:list[complex]) -> list[complex]:
+    """------------------------------------------------------------------------
+    Simulate a 3 input beam split and mix.
+    
+    Parameters
+    ----------
+    - `beams` : List of input beams complex amplitudes 
+    
+    Returns
+    -------
+    - List of output beams complex amplitudes
+        --------------------------------------------------------------------"""
+    
+    phi = np.pi/4
+    S = 1/np.sqrt(4) * np.array([
+        [1               , np.exp(1j*phi)  , 0             ],
+        [-np.exp(-1j*phi), 1               , 0             ],
+        [1               , 0               , np.exp(1j*phi)],
+        [-np.exp(-1j*phi), 0               , 1             ],
+        [0               , 1               , np.exp(1j*phi)],
+        [0               , -np.exp(-1j*phi), 1             ]
+    ])
+
+    return S @ beams
+
+def splitmix_2x2(beams:np.array, theta=np.pi/2) -> np.array:
+    """------------------------------------------------------------------------
+    Simulate a 2 input beam split and mix.
+
+    Parameters
+    ----------
+    - `beams` : Array of 2 input beams complex amplitudes
+    - `theta` : Phase shift between the two output beams
+
+    Returns
+    -------
+    - Array of 2 output beams complex amplitudes
+    ------------------------------------------------------------------------"""
+
+    S = 1/np.sqrt(2) * np.array([
+        [np.exp(1j*theta/2), np.exp(-1j*theta/2)],
+        [np.exp(-1j*theta/2), np.exp(1j*theta/2)]
+    ])
+
+    return S @ beams
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def optimize(kn0, beam, verbose = False):
 
     shifts = np.zeros(14)
@@ -11,7 +167,7 @@ def optimize(kn0, beam, verbose = False):
     if verbose: print("+"*10, " Phase 1 ", "+"*10)
     delta = 1
     bright_evol = []
-    while delta > 1e-20:
+    while delta > 1e-2:
         delta /= 1.5
         if verbose: print("\n==========\n")
         p = [1,2,3,4,5,7] # shifters that contribute to redirecting light to the bright output
@@ -60,7 +216,7 @@ def optimize(kn0, beam, verbose = False):
     if verbose: print("+"*10, " Phase 2 ", "+"*10)
     delta = 1
     dark_symmetry_evol = []
-    while delta > 1e-20:
+    while delta > 1e-2:
         delta /= 1.5
         if verbose: print("\n==========\n")
         p = [6,8,9,10,11,12,13,14] # shifters that contribute to the symmetry of the dark outputs
@@ -149,15 +305,15 @@ def scan(kn, beams, scan_on=(1,2), initial_parameters=None, optimized_parameters
 
     # Scan shift power parameter space
     scan = np.linspace(-1, 1, 101, endpoint=True)
-    null_depths = np.zeros((6, len(scan), len(scan)))
-    brights = np.zeros((len(scan), len(scan)))
-    first_layer_nulls = np.zeros((4, len(scan), len(scan)))
-    second_layer_nulls = np.zeros((4, len(scan), len(scan)))
+    darks_map = np.zeros((6, len(scan), len(scan)))
+    bright_map = np.zeros((len(scan), len(scan)))
 
     if plot_intermediate_states:
-        fig_b, axs_b = plt.subplots(2,4, figsize=(20,10))
+        first_layer_nulls = np.zeros((4, len(scan), len(scan)))
+        second_layer_nulls = np.zeros((4, len(scan), len(scan)))
 
-    fig, axs = plt.subplots(3,3, figsize=(20,15))
+        _, axs_b = plt.subplots(2,4, figsize=(20,10))
+    _, axs = plt.subplots(4,3, figsize=(20,25))
 
     if initial_parameters is not None:
         parameters = np.array(initial_parameters).astype(float)
@@ -181,8 +337,8 @@ def scan(kn, beams, scan_on=(1,2), initial_parameters=None, optimized_parameters
                     second_layer_nulls[k,i,j] = np.abs(null)**2
 
             for k, dark in enumerate(darks):
-                null_depths[k,i,j] = np.abs(dark)**2 # np.log(np.abs(dark)**2 / np.abs(bright)**2)
-                brights[i,j] = np.abs(bright)**2
+                darks_map[k,i,j] = np.abs(dark)**2
+                bright_map[i,j] = np.abs(bright)**2
 
     if plot_intermediate_states:
         for k in range(4):
@@ -204,31 +360,64 @@ def scan(kn, beams, scan_on=(1,2), initial_parameters=None, optimized_parameters
     for k in range(6):
         p = axs[k%2, k//2]
         p.set_title(f"Dark {k+1}")
-        im = p.imshow(null_depths[k], extent=[-1, 1, -1, 1])
+        im = p.imshow(darks_map[k], extent=[-1, 1, -1, 1])
         if optimized_parameters is not None: p.scatter(optimized_parameters[scan_on[1]-1], optimized_parameters[scan_on[0]-1], color='red')
         p.set_xlabel(f"Parameter {scan_on[1]}")
         p.set_ylabel(f"Parameter {scan_on[0]}")
         plt.colorbar(im)
 
-    p = axs[2,0]
-    p.set_title(f"Mean of null depths")
-    im = p.imshow(np.mean(null_depths, axis=0), extent=[-1, 1, -1, 1])
-    if optimized_parameters is not None: p.scatter(optimized_parameters[scan_on[1]-1], optimized_parameters[scan_on[0]-1], color='red')
-    p.set_xlabel(f"Parameter {scan_on[1]}")
-    p.set_ylabel(f"Parameter {scan_on[0]}")
-    plt.colorbar(im)
+    # Plot diff of dark pairs -------------------------------------------------
+        
+    diff1 = darks_map[0] - darks_map[1]
+    diff2 = darks_map[2] - darks_map[3]
+    diff3 = darks_map[4] - darks_map[5]
 
-    p = axs[2,2]
-    p.set_title(f"Std of null depths")
-    im = p.imshow(np.std(null_depths, axis=0), extent=[-1, 1, -1, 1])
+    # Compute max diff
+    m = np.max([np.max(np.abs(diff1)), np.max(np.abs(diff2)), np.max(np.abs(diff3))])
+
+    p = axs[2,0]
+    p.set_title(f"Dark 1 - Dark 2")
+    im = p.imshow(diff1, extent=[-1, 1, -1, 1], cmap='RdBu', vmin=-m, vmax=m)
     if optimized_parameters is not None: p.scatter(optimized_parameters[scan_on[1]-1], optimized_parameters[scan_on[0]-1], color='red')
     p.set_xlabel(f"Parameter {scan_on[1]}")
     p.set_ylabel(f"Parameter {scan_on[0]}")
     plt.colorbar(im)
 
     p = axs[2,1]
+    p.set_title(f"Dark 3 - Dark 4")
+    im = p.imshow(diff2, extent=[-1, 1, -1, 1], cmap='RdBu', vmin=-m, vmax=m)
+    if optimized_parameters is not None: p.scatter(optimized_parameters[scan_on[1]-1], optimized_parameters[scan_on[0]-1], color='red')
+    p.set_xlabel(f"Parameter {scan_on[1]}")
+    p.set_ylabel(f"Parameter {scan_on[0]}")
+    plt.colorbar(im)
+
+    p = axs[2,2]
+    p.set_title(f"Dark 5 - Dark 6")
+    im = p.imshow(diff3, extent=[-1, 1, -1, 1], cmap='RdBu', vmin=-m, vmax=m)
+    if optimized_parameters is not None: p.scatter(optimized_parameters[scan_on[1]-1], optimized_parameters[scan_on[0]-1], color='red')
+    p.set_xlabel(f"Parameter {scan_on[1]}")
+    p.set_ylabel(f"Parameter {scan_on[0]}")
+    plt.colorbar(im)
+
+    p = axs[3,0]
+    p.set_title(f"Mean of null depths")
+    im = p.imshow(np.mean(darks_map, axis=0), extent=[-1, 1, -1, 1])
+    if optimized_parameters is not None: p.scatter(optimized_parameters[scan_on[1]-1], optimized_parameters[scan_on[0]-1], color='red')
+    p.set_xlabel(f"Parameter {scan_on[1]}")
+    p.set_ylabel(f"Parameter {scan_on[0]}")
+    plt.colorbar(im)
+
+    p = axs[3,2]
+    p.set_title(f"Std of null depths")
+    im = p.imshow(np.std(darks_map, axis=0), extent=[-1, 1, -1, 1])
+    if optimized_parameters is not None: p.scatter(optimized_parameters[scan_on[1]-1], optimized_parameters[scan_on[0]-1], color='red')
+    p.set_xlabel(f"Parameter {scan_on[1]}")
+    p.set_ylabel(f"Parameter {scan_on[0]}")
+    plt.colorbar(im)
+
+    p = axs[3,1]
     p.set_title(f"Bright")
-    im = p.imshow(brights, extent=[-1, 1, -1, 1])
+    im = p.imshow(bright_map, extent=[-1, 1, -1, 1])
     if optimized_parameters is not None: p.scatter(optimized_parameters[scan_on[1]-1], optimized_parameters[scan_on[0]-1], color='red')
     p.set_xlabel(f"Parameter {scan_on[1]}")
     p.set_ylabel(f"Parameter {scan_on[0]}")
