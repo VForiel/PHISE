4516e36d9f6979e26e3abce0917defa504baaed4
Vincent Foriel <vince.lrf@gmail.com>
2024-08-01 14:31:23 +0200

    Reoved useless files

--- src/telescope.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import astropy.units as u
-import numba as nb
-import matplotlib.pyplot as plt
-from io import BytesIO
-import ipywidgets as widgets
-from IPython.display import display
--- src/utils.py
+++
@@ -1,5 +0,0 @@
-import numba as nb
-import numpy as np
-import astropy.units as u
-
-
